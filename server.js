8f482a67cfc9277c849b587ac8c007727ca25eed
qige2016 <286882998@qq.com>
2017-12-17 13:55:13 +0800

    增加的查看在线用户和一些性能优化

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 //服务器及页面部分
 var express = require('express'),
     app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
     users = [];//保存所有在线用户的昵称
-//specify the html we will use
-app.use('/', express.static(__dirname + '/www'));
-//bind the server to the 80 port
-//server.listen(3000);//for local test
-server.listen(process.env.PORT || 3000);//publish to heroku
-//server.listen(process.env.OPENSHIFT_NODEJS_PORT || 3000);//publish to openshift
-//console.log('server started on port'+process.env.PORT || 3000);
+    port = process.env.PORT || 3000;
+    server = app.listen(port),
+    io = require('socket.io').listen(server)
+
+app.use('/', express.static(__dirname + '/static'));
+
 //socket部分
 io.sockets.on('connection', function(socket) {
     //昵称设置
@@ -18,28 +15,26 @@ io.sockets.on('connection', function(socket) {
         if (users.indexOf(nickname) > -1) {
             socket.emit('nickExisted');
         } else {
-            //socket.userIndex = users.length;
             socket.nickname = nickname;
             users.push(nickname);
             socket.emit('loginSuccess');
-            io.sockets.emit('system', nickname, users.length, 'login');//向所有连接到服务器的客户端发送当前登录用户的昵称
+            io.sockets.emit('system', nickname, users, users.length, 'login');//向所有连接到服务器的客户端发送用户信息
         };
     });
     //断开连接的事件
     socket.on('disconnect', function() {
         if (socket.nickname != null) {
-            //users.splice(socket.userIndex, 1);
             //将断开连接的用户从users中删除
             users.splice(users.indexOf(socket.nickname), 1);
             //通知除自己以为的所有人
-            socket.broadcast.emit('system', socket.nickname, users.length, 'logout');
+            socket.broadcast.emit('system', socket.nickname, users, users.length, 'logout');
         }
     });
-    //接收新信息
+    //接收新信息则广播其他人
     socket.on('postMsg', function(msg, color) {
         socket.broadcast.emit('newMsg', socket.nickname, msg, color);
     });
-    //接收用户发来的图片
+    //接收用户发来的图片则广播其他人
     socket.on('img', function(imgData, color) {
         //通过一个newImg事件分发到除自己外的每个用户
         socket.broadcast.emit('newImg', socket.nickname, imgData, color);
